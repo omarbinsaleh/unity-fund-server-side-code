970a7362cb5ac15c1424a71ad9ac414a5e20967b
omarbinsaleh <omarbinsaleh44@gmail.com>
2024-12-04 22:16:42 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares:
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+   console.log(`Unity Fund Server is running on port: ${port}`);
+})
